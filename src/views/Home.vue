9dadcba320084c35b1fc9df905c6a79b5787a90a
Anikin Yuri <anikin.yu@gmail.com>
2021-11-21 23:07:49 +0300

    config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+.home
+  img(src='../assets/logo.png' alt='Vue logo')
+  HelloWorld(msg='Welcome to Your Vue.js + TypeScript App')
 </template>
 
 <script lang="ts">
