74f25192a3c0b735b227e78cdca1912cb2555c73
y.anikin <y.anikin@tochno.pro>
2021-11-22 10:41:14 +0300

    fonts

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template lang="pug">
-#nav
-  router-link(to='/') Home |
-  router-link(to='/about') About
-router-view
+.global-wrapper
+  router-view
 </template>
 
 <style>
+  @import './common/common.css';
+
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: var(--font-primary);
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
   }
 
-  #nav {
-    padding: 30px;
-  }
-
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  #nav a.router-link-exact-active {
-    color: #42b983;
+  .global-wrapper {
+    position: relative;
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
   }
 </style>
