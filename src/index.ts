28fb913c9052e69cfdd065223610141b29a75dbd
y.anikin <y.anikin@tochno.pro>
2021-11-23 15:52:42 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
+import { App as Application, Plugin } from 'vue';
+import { setVueInstance } from './utils/setVueInstance';
 import Button from './components/button/Button.vue';
-export default Button;
+
+const components = {
+  Button,
+};
+
+const install: Exclude<Plugin['install'], undefined> = (instance: Application) => {
+  setVueInstance(instance);
+  for (const componentKey in components) {
+    instance.use((components as any)[componentKey]);
+  }
+};
+
+export default install;
