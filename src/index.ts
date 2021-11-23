edd3dec310d7da4718e4a8e97eeb33aff434bbda
y.anikin <y.anikin@tochno.pro>
2021-11-23 14:36:15 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export { Button } from './components/button/Button';
+import Vue from 'vue';
+import Button from './components/button/Button';
+const Components = {
+  Button,
+};
+
+const app = Vue.createApp({});
+
+Object.keys(Components).forEach((name) => {
+  // app.component(name, Components[name])
+});
+
+export default Components;
