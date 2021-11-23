88d2f588cc93d873b67883747904338d95c68fd9
y.anikin <y.anikin@tochno.pro>
2021-11-23 15:33:28 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import Vue from 'vue';
 import Button from './components/button/Button.vue';
-const Components = {
-  Button,
-};
-
-const app = Vue.createApp({});
-
-Object.values(Components).forEach((component) => {
-  app.component(component.name, component);
-});
-
-export default Components;
+export default Button;
