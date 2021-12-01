251dd04b85151e2c53cbb22374fd711ddf1e02e4
Anikin Yuri <anikin.yu@gmail.com>
2021-12-01 16:29:36 +0300

    TableDataSource.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,34 @@
 import { DefineComponent } from "vue";
-import TableDataSource from './TableDataSource'
+
+export type TableRow = Record<string, unknown>;
+export type TableDataRow<T> = T & { checked: boolean; _id: number };
+export type TableDataMapper<T> = (item: IVoxTableDataRow) => IVoxTableDataRow<T>;
+
+export declare class TableDataSource<T> {
+  perPage: number;
+  offset: number;
+  private _data!: TableDataRow<T>[];
+
+  constructor(data: T[], mapper?: TableDataMapper<T>);
+
+  get total(): number;
+
+  get tableData(): TableDataRow<T>[];
+
+  get pageData(): TableDataRow<T>[];
+
+  get totalPages(): number;
+
+  get selectedItems(): TableDataRow<T>[];
+
+  get areAllSelected(): boolean;
+
+  selectAll(): void;
+
+  unSelectAll(): void;
+
+  removeRow(id: number): void;
+}
 
 export const BottomBar: DefineComponent;
 export const FixedBottomBar: DefineComponent;
@@ -36,7 +65,6 @@ export const ProgressBar: DefineComponent;
 export const useProgressBar: any;
 export const Select: DefineComponent;
 export const Table: DefineComponent;
-export const TableDataSource: TableDataSource;
 export const Tabs: DefineComponent;
 export const Textarea: DefineComponent;
 export const Timer: DefineComponent;
