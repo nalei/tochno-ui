90282f83d642d4ef1ebb632d74fd7c601f15fb16
Ivan Fedyshin deb10@p8p67 <fixmates@gmail.com>
2021-12-03 11:43:30 +0300

    complete

--- types/src/components/button/Button.d.ts
+++ types/src/components/button/Button.d.ts
@@ -12,24 +12,3 @@ export default class Button extends Vue {
   loading?: boolean;
   iconOnly?: boolean;
 }
-
-
-// import { DefineComponent } from 'vue';
-
-// export type ButtonSize = 's' | 'm' | 'l';
-
-// export type ButtonMode = 'primary' | 'secondary' | 'info' | 'state';
-
-// declare const Button: DefineComponent<
-//   {
-//     size?: ButtonSize;
-//     mode?: ButtonMode;
-//     icon?: Icon;
-//     loading?: boolean;
-//     iconOnly?: boolean;
-//   },
-//   {},
-//   any
-// >;
-
-// export default Button;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-// import typescript2 from 'rollup-plugin-typescript2';
 import dts from 'vite-plugin-dts';
-// import GenerateExports from 'vite-plugin-generate-exports'
 
 
 export default defineConfig({
@@ -16,49 +14,13 @@ export default defineConfig({
         outputDir: './types',
       })
     },
-    // {
-    //   ...typescript2(),
-    //   apply: 'build',
-    // },
-    // GenerateExports({
-    //   patterns: [
-    //     {
-    //       matchTokens: ['// Start_Exports', '// End_Exports'],
-    //       files: 'src/**/*.vue'
-    //     }
-    //   ]
-    // }),
-    // {
-    //   apply: 'build',
-    //   ...typescript2({
-    //     // tsconfig: './tsconfig.json',
-    //     tsconfig: resolve(__dirname, `tsconfig.json`),
-    //     check: true,
-    //     // include: ["types/*"],
-    //     // useTsconfigDeclarationDir: true,
-    //   }),
-    //   enforce: 'pre',
-    // },
   ],
   build: {
     assetsDir: 'public',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'ui',
-      // formats: ['es'],
     },
-    // rollupOptions: {
-    //   external: ['vue'],
-    //   input: {
-    //     lib: resolve(__dirname, 'src/index.ts'),
-    //     types: resolve(__dirname, 'types/index.ts'),
-    //   },
-    //   output: {
-    //     globals: {
-    //       vue: 'Vue',
-    //     },
-    //   },
-    // },
     minify: true,
   },
   resolve: {
