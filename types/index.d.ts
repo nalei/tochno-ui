8602e659ba3623d10a3be239cfc21420774ef5f8
Anikin Yuri <anikin.yu@gmail.com>
2021-12-03 16:13:05 +0300

    TableRow TableDataRow

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,4 @@
 export { DropdownOptionProps } from './src/components/dropdown/DropdownOption';
 export { ISortedListRow } from './src/components/lists/sorted-list/SortedList';
+export { TableRow, TableDataRow } from './src/components/table/Table';
 export * from './src';
