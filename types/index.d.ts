db6244913a12b0fdad5a011fb9911674a5325806
Юрий Аникин <y.anikin@tochno.pro>
2021-12-06 16:53:03 +0300

    TabsItem

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 export { DropdownOptionProps } from './src/components/dropdown/DropdownOption';
 export { ISortedListRow } from './src/components/lists/sorted-list/SortedList';
-export { TableRow, TableDataRow } from './src/components/table/Table';
+export { TableRow, TableDataRow, TableColumn } from './src/components/table/Table';
 export { ListOption } from './src/components/menu/ListOption';
 export { MenuMode } from './src/components/menu/MenuList';
+export { TabsItem } from './src/components/tabs/Tabs';
 export * from './src';
