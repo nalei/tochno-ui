60e83ecb00ec5167dc07e8eedece10f8990544e5
Юрий Аникин <y.anikin@tochno.pro>
2021-12-07 12:55:45 +0300

    Svg sprite

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,9 @@
 <template lang="pug">
 .h-500 Coming soon
-  Button(:icon='{ name: "ic24-plus" }') Coming soon
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import Button from '@/components/button/Button.vue';
 
-  export default defineComponent({
-    components: { Button },
-  });
+  export default defineComponent({});
 </script>
